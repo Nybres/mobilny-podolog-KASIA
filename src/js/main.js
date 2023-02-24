efe9ce0106ca11e37b6050093e653c4b8dcf664f
Pawel <pawelserbyn@wp.pl>
2023-02-24 19:57:03 +0100

    add mobile navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,34 @@
-const hamburgerBtn = document.querySelector(".hamburger");
-// const ctaBtn = document.querySelector(".navigation__cta");
 const menu = document.querySelector(".navigation__menu");
+const hamburgerBtn = document.querySelector(".hamburger");
+const submenu = document.querySelector(".navigation__submenu");
+const menuItemArrow = document.querySelector(".navigation__nav-arrow-box");
+const menuBackBox = document.querySelector(".navigation__submenuback");
+const menuBackArrow = document.querySelector(".navigation__submenuback-arrow");
 
 const handleNav = params => {
   menu.classList.toggle("navigation__menu--active");
+  hamburgerBtn.classList.toggle("hamburger--active");
+};
+
+const handleSubmenu = params => {
+  submenu.classList.add("navigation__submenu--active");
+  menuBackBox.classList.add("navigation__submenuback--active");
+};
+
+const handleSubemnuBack = params => {
+  submenu.classList.remove("navigation__submenu--active");
+  menuBackBox.classList.remove("navigation__submenuback--active");
 };
 
 hamburgerBtn.addEventListener("click", () => {
   handleNav();
+  handleSubemnuBack();
+});
+
+menuItemArrow.addEventListener("click", () => {
+  handleSubmenu();
+});
+
+menuBackArrow.addEventListener("click", () => {
+  handleSubemnuBack();
 });
