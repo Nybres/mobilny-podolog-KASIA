5eb0be26a346d793f05c9ac5b6d35496b2226d42
Pawel <pawelserbyn@wp.pl>
2023-02-24 22:49:50 +0100

    add desktop navigation

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,15 @@ const submenu = document.querySelector(".navigation__submenu");
 const menuItemArrow = document.querySelector(".navigation__nav-arrow-box");
 const menuBackBox = document.querySelector(".navigation__submenuback");
 const menuBackArrow = document.querySelector(".navigation__submenuback-arrow");
+const overlay = document.querySelector(".overlay");
+
+const addOverlay = params => {
+  overlay.classList.add("overlay--active");
+};
+
+const removeOverlay = params => {
+  overlay.classList.remove("overlay--active");
+};
 
 const handleNav = params => {
   menu.classList.toggle("navigation__menu--active");
@@ -32,3 +41,51 @@ menuItemArrow.addEventListener("click", () => {
 menuBackArrow.addEventListener("click", () => {
   handleSubemnuBack();
 });
+
+if (window.innerWidth > 992) {
+  const submenuInner = document.querySelector(".navigation__submenu-inner");
+  const submenuElements = document.querySelectorAll(
+    ".navigation__submenu-item"
+  );
+  let menuTimer = !1;
+
+  menuItemArrow.addEventListener("mouseover", () => {
+    if (menuTimer) {
+      clearTimeout(menuTimer);
+    }
+    submenu.classList.add("navigation__submenu--hover");
+    addOverlay();
+  });
+
+  menuItemArrow.addEventListener("mouseout", () => {
+    menuTimer = setTimeout(() => {
+      submenu.classList.remove("navigation__submenu--hover");
+      removeOverlay();
+    }, 400);
+  });
+
+  submenuInner.addEventListener("mouseout", () => {
+    if (menuTimer) {
+      clearTimeout(menuTimer);
+    }
+    submenu.classList.add("navigation__submenu--hover");
+    addOverlay();
+  });
+
+  submenuInner.addEventListener("mouseout", () => {
+    menuTimer = setTimeout(() => {
+      submenu.classList.remove("navigation__submenu--hover");
+      removeOverlay();
+    }, 400);
+  });
+
+  submenuElements.forEach(el => {
+    el.addEventListener("mouseover", () => {
+      if (menuTimer) {
+        clearTimeout(menuTimer);
+      }
+      submenu.classList.add("navigation__submenu--hover");
+      addOverlay();
+    });
+  });
+}
