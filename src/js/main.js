9ab21928d245877a8c3a9285b1a7d4a0f8e8ef3d
Pawel <pawelserbyn@wp.pl>
2023-03-04 20:04:55 +0100

    add faq section on mobile

--- src/js/main.js
+++ src/js/main.js
@@ -118,3 +118,15 @@ if (window.innerWidth > 992) {
     });
   });
 }
+
+//faq
+const faqContainer = document.querySelector(".faq__container");
+faqContainer.addEventListener("click", e => {
+  const box = e.target.closest(".faq__box");
+  const boxQuestion = box.querySelector(".faq__box-question");
+  const boxAnswer = box.querySelector(".faq__box-answer");
+
+  box.classList.toggle("faq__box--active");
+  boxQuestion.classList.toggle("faq__box-question--active");
+  boxAnswer.classList.toggle("faq__box-answer--active");
+});
